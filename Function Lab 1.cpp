384ad295bf3401c2ab634bfcbe3bf36ab13e1865
ngmont <16mawise@gmail.com>
2016-05-27 14:56:56 -0400

    Update Function Lab 1.cpp

--- Function Lab 1.cpp
+++ Function Lab 1.cpp
@@ -1,8 +1,8 @@
 //********************************************************************
 // *Program Name   :  Function Lab 1
- //*Author         :  Natalia Montilla
- //*Date           :  April 20th, 2016
- //*Course/Section :  CSC110-002
+//*Author         :  Natalia Montilla
+//*Date           :  April 20th, 2016
+//*Course/Section :  CSC110-002
 //*Program Description: Ask the user to input ‘A’ for absolute value or ‘P’ for 
 //*power or ‘Q’ for quit.  As long as the user didn’t quit then input two numbers.  
 //*If they chose ‘A’ then find the absolute value of (the first number minus the second 
@@ -36,6 +36,8 @@
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
+#include <string>
+#include <locale>
 using namespace std;
 
 int main()
@@ -43,12 +45,12 @@ int main()
 	//local constants
     string P;          //power operation
     string A;          //absolute power operation
-    int Q = 0;        //sentinal value
+    string Q = 0;        //sentinal value
 
 	//local variables
     int Num_1;         //first number entered
     int Num_2;         //second number entered
-    string Opp;        //operation chosen
+    char Opp;        //operation chosen
     int Ans;           //answer
 
        /**************************start main program*********************/
@@ -59,12 +61,12 @@ int main()
        cout << "Input operation Abs Value (A), Power (P) or QUIT (Q): "; 
        cin  >> Opp; 
        //check to see if Opp is uppercase (if not, we'll convert to uppercase)
-       
-       //Convert Operation to uppercase
-       char toupper(Opp); 
+       if(islower(Opp))
+          { //Convert Operation to uppercase
+           Opp = toupper(Opp); }
 
        // WHILE (Operation != QUIT)
-       while (Opp != QUIT){
+       while (Opp == 'Q'){
 
             //Input 2 #'s
        	    cout << "Enter first number: ";
@@ -73,25 +75,25 @@ int main()
        	    cin  >> Num_2; 
 
             //IF (Operations Power)
-            if (Opp == P){ 
-
+            if (Opp == 'P'){ 
+            
                //Answer = 1st # raised to 2nd power)
-               Ans = pow (Num_1, Num_2); 
-                         }
+               Ans = pow(Num_1, Num_2); 
+                                       }
             //ELSE opperator is power
-            else if (Opp == A){
-
+            else if (Opp == 'A'){
+                
                 //Answer = Absolute VAlue of 1st - 2nd
-                Ans = abs(Num_1) - abs(Num_2); 
+                Ans = abs((Num_1) - (Num_2)); 
                                } //END IF
             //Dispaly Answers
             cout << "Answer is: " << Ans << endl; 
-                               
+                              
             //ELSE 
             else
-
+            {
             //Dispaly Error	
-            coud << "Error"; 
+            cout << "Error"; }
             	
        //Input Operation
        cout << "Input Opperation Abs Value (A), Power (P) or QUIT (Q): "; 
@@ -99,7 +101,8 @@ int main()
 
        //Convert Operation to uppercase
        char toupper (Opp); 
-
-    }//END WHILE
-        
+    }   
+    //END WHILE
+    
+} 
 //END Function Lab	
